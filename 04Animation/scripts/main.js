e92de4bbc80b4ee9c411f210215ca1aaf8de1b4e
anoukxjansma <61313116+anoukxjansma@users.noreply.github.com>
2020-05-14 18:38:11 +0200

    Add files via upload

---
+++ 04Animation/scripts/main.js
@@ -0,0 +1,12 @@
+// //changing individual properties with code and using setInterval
+ var rotationSpeed = 0.3;
+ var myOtherBox = document.getElementById('myOtherBox');
+
+function spin(){
+ 	myOtherBox.object3D.rotation.x -= rotationSpeed;
+ 	myOtherBox.object3D.rotation.y -= rotationSpeed;
+
+ 	console.log(myOtherBox.object3D.rotation.x);
+ }
+
+ setInterval(spin, 16); //equivalent to 60 fps
